532447182a19970a01d179ea8d8ddec8740a0137
balyberd <balyberd@gmail.com>
2017-03-22 23:57:50 +0300

    no message

--- Models/Task.cs
+++ Models/Task.cs
@@ -12,11 +12,18 @@ namespace KV.Models
         public string Name { get; set; }            // краткое название задачи
         public string Desription { get; set; }      // описание задачи
         public int StatusId { get; set; }           // статус задачи
-        public TaskStatus Status { get; set; }      // статус по справочнику TaskStatus   
+        public TaskStatus Status { get; set; }      // статус по справочнику TaskStatus  
+         
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime CreateDate { get; set; }    // Дата создания
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime StartDate { get; set; }     // Дата начала выполнения
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime EndDate { get; set; }       // Дата окончания выполнения
     }
 }
